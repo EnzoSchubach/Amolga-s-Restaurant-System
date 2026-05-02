38393c7613492c5c547282c5434ae48799f3009d
Enzo Schubach <enzoschubachcasanova@gmail.com>
2026-05-01 22:40:29 -0300

    feat: add add, rmv, list and find item methods into Cardapio class

--- mavenproject1/src/main/java/com/amolga/mavenproject1/Cardapio.java
+++ mavenproject1/src/main/java/com/amolga/mavenproject1/Cardapio.java
@@ -4,10 +4,34 @@
  */
 package com.amolga.mavenproject1;
 
+import java.util.*;
+
 /**
  *
  * @author enzoschubach
  */
 public class Cardapio {
+    private List<ItemCardapio> items;
     
+    public void addItem(){
+        
+    }
+    
+    public void rmvItem(){
+        
+    }
+    
+    public void findItem(){
+        
+    }
+    
+    public void listItems(){
+        int cont = 0;
+        
+        for(ItemCardapio item : items){
+            cont++;
+            System.out.println("Item n" + cont + ": " + item);
+        }
+    }
 }
+
